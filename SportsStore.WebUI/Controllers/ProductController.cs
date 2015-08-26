fbe99b0140d29eaae528150fa0358df65d44a7eb
Den <denizdeniz90@gmail.com>
2015-08-26 15:46:54 +0300

    Add controller and View in webUi

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        // GET: Product
+        public ProductController( IProductRepository repositoryProcut)
+        {
+            this.repository = repositoryProcut;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+
+    }
+}
