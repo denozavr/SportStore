12d5a1d7a23e022fe9c944a530e9d506e23abe4f
Den <denizdeniz90@gmail.com>
2015-09-03 16:52:15 +0300

    Add cartViewModel

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -16,6 +17,16 @@ namespace SportsStore.WebUI.Controllers
         {
             repository = repo;
         }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
         // GET: Cart
         public ActionResult AddToCart(int productId, string returnUrl)
         {
