d33c2edd62d2b7976094e70be07293bf5e15db0b
Den <denizdeniz90@gmail.com>
2015-09-17 16:31:15 +0300

    Add delete action and code in domain

--- SportsStore.Domain/Abstract/IProductRepository.cs
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -11,5 +11,7 @@ namespace SportsStore.Domain.Abstract
     {
         IEnumerable<Product> Products { get; }
         void SaveProduct(Product product);
+
+        Product DeleteProduct(int productId);
     }
 }
