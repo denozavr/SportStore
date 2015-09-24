19c1f61dc2b6565be2a134af9decc302612fab5e
Den <denizdeniz90@gmail.com>
2015-09-24 12:58:09 +0300

    add getImage action (prod contrloller)

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -33,5 +33,19 @@ namespace SportsStore.WebUI.Controllers
             return View(model);
         }
 
+
+        public FileContentResult GetImage(int productId)
+        {
+            Product prod = repository.Products.FirstOrDefault(p => p.ProductId == productId);
+            if (prod != null)
+            {
+                return File(prod.ImageData, prod.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
     }
 }
