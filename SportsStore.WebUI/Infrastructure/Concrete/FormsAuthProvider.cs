6529fe0a667d95bd20511c8e7e0fcb91510bf768
Den <denizdeniz90@gmail.com>
2015-09-24 11:33:09 +0300

    Add and register AuthProvider

---
+++ SportsStore.WebUI/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using SportsStore.WebUI.Infrastructure.Abstract;
+
+namespace SportsStore.WebUI.Infrastructure.Concrete
+{
+    public class FormsAuthProvider :IAuthProvider
+    {
+        public bool Authenticate(string name, string password)
+        {
+            bool result = FormsAuthentication.Authenticate(name, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(name, false);
+            }
+            return result;
+        }
+    }
+}
